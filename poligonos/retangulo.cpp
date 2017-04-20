4fe6f7a66ed84904c55d7324afd900a8d3472a6a
Pedro <pedrounix@outlook.com.br>
2017-04-20 15:55:08 -0300

    ajuste retangulo

--- poligonos/retangulo.cpp
+++ poligonos/retangulo.cpp
@@ -4,6 +4,6 @@
 Retangulo::Retangulo(float x, float y, float largura, float altura){
     retangulo.setVertice(x,y);
     retangulo.setVertice(x - largura,y);
-    retangulo.setVertice(x,y-altura);
     retangulo.setVertice(x - largura, y - altura);
+    retangulo.setVertice(x,y-altura);
 }
